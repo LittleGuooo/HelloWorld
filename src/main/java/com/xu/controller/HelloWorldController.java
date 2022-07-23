ca48f6de9151b2e10a8efb5929a3e6e8e1f2d4fb
LittleBai <945807515@qq.com>
2022-07-23 10:22:11 +0800

    添加了controller。

--- src/main/java/com/xu/controller/HelloWorldController.java
+++ src/main/java/com/xu/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.xu.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,14 @@ public class HelloWorldController {
     @GetMapping
     public String hello(HttpServletRequest httpServletRequest){
         StringBuffer requestURL = httpServletRequest.getRequestURL();
-        System.out.println("当前访问路径：" + requestURL);
+        System.out.println("Request URL：" + requestURL);
         return "Hello, World!";
+    }
+
+    @GetMapping("/{name}")
+    public String helloWithName(HttpServletRequest httpServletRequest, @PathVariable String name){
+        StringBuffer requestURL = httpServletRequest.getRequestURL();
+        System.out.println("Request URL：" + requestURL);
+        return "Hello, " + name + "!";
     }
 }
